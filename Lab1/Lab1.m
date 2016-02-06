2f34de5ded0fe54f093b53779acd02b764626855
Karan Thukral <karanthukral@gmail.com>
2016-02-06 12:11:56 -0500

    Fixed contours calc

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -16,18 +16,18 @@ covar_d = [8 0; 0 8];
 mu_e = [10 5];
 covar_e = [10 -5; -5 20];
 
-A = gaussianTransform(mu_a, covar_a)
-B = gaussianTransform(mu_b, covar_b)
-C = gaussianTransform(mu_c, covar_c)
-D = gaussianTransform(mu_d, covar_d)
-E = gaussianTransform(mu_e, covar_e)
+A = gaussianTransform(mu_a, covar_a);
+B = gaussianTransform(mu_b, covar_b);
+C = gaussianTransform(mu_c, covar_c);
+D = gaussianTransform(mu_d, covar_d);
+E = gaussianTransform(mu_e, covar_e);
 
-[A_x, A_y] = contourCalculation(mu_a, covar_a)
-[B_x, B_y] = contourCalculation(mu_b, covar_b)
-[C_x, C_y] = contourCalculation(mu_c, covar_c)
-[D_x, D_y] = contourCalculation(mu_d, covar_d)
-[E_x, E_y] = contourCalculation(mu_e, covar_e)
+[A_x, A_y] = contourCalculation(mu_a, covar_a);
+[B_x, B_y] = contourCalculation(mu_b, covar_b);
+[C_x, C_y] = contourCalculation(mu_c, covar_c);
+[D_x, D_y] = contourCalculation(mu_d, covar_d);
+[E_x, E_y] = contourCalculation(mu_e, covar_e);
 
-scatter(A(:,1),A(:,2),'filled');
+scatter(E(:,1),E(:,2),'filled');
 hold on
-plot(A_x,A_y,'LineWidth',2);
+plot(E_x,E_y,'LineWidth',2);
--- Lab1/contourCalculation.m
+++ Lab1/contourCalculation.m
@@ -1,12 +1,12 @@
 %CONTOURCALCULATION Summary of this function goes here
 %   Detailed explanation goes here
-function [x, y] = contourCalculation( mean, covar )
+function [X, Y] = contourCalculation( mean, covar )
     [sigma, correlation] = cov2corr(covar);
     majorAxis = 2*sigma(1);
     minorAxis = 2*correlation(2);
     centerX = mean(1);
     centerY = mean(2);
-    [V,D,W] = eig(sigma);
+    [V,D,W] = eig(covar);
     orientation = tan(W(2,1)/W(1,1))*360/(2*pi);
 
     theta = 0 : 0.05 : 2*pi;
@@ -14,7 +14,7 @@ function [x, y] = contourCalculation( mean, covar )
     xx = (majorAxis/2) * sin(theta) + centerX;
     yy = (minorAxis/2) * cos(theta) + centerY;
 
-    x = (xx-centerX)*cos(orientation) - (yy-centerY)*sin(orientation) + centerX;
-    y = (xx-centerX)*sin(orientation) + (yy-centerY)*cos(orientation) + centerY;
+    X = (xx-centerX)*cos(orientation) - (yy-centerY)*sin(orientation) + centerX;
+    Y = (xx-centerX)*sin(orientation) + (yy-centerY)*cos(orientation) + centerY;
 end
 
